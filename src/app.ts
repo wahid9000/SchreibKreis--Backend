59bf1c809216197a70a605bee11fcb2a9d542851
wahid_9000 <wahidchowdhury9000@gmail.com>
2026-08-18 19:38:31 +0200

    feat: add cors middleware for handling cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@ import { postRouter } from "./modules/post/post.router";
 import { globalErrorHandler } from "./middleware/errorHandler";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app: express.Application = express();
+app.use(express.json({ limit: "10kb" }));
+
+app.use(cors({
+  origin: process.env.APP_URL || "http://localhost:5000",
+  credentials: true,
+}));
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
-
-app.use(express.json({ limit: "10kb" }));
 
 app.use("/posts", postRouter);
 
