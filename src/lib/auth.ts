54944b11ad70a659b9b709c6623e6f9bf3899a0c
wahid_9000 <wahidchowdhury9000@gmail.com>
2026-08-19 01:24:17 +0200

    update: Google social provider configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -61,8 +61,10 @@ export const auth = betterAuth({
     },
     socialProviders: {
       google: {
+        prompt: "select_account consent",
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        accessType: "offline",
       },
     },
   },
