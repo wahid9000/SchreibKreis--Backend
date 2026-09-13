6ce00c9689287d7d24077e856b37a5012858ff1e
wahid_9000 <wahidchowdhury9000@gmail.com>
2026-09-14 01:42:34 +0200

    feat: update authentication configuration to include APP_URL in trustedOrigins and refine socialProviders setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,30 +12,32 @@ export const auth = betterAuth({
   }),
 
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: [process.env.BETTER_AUTH_URL!],
-  user: {
-    additionalFields: {
-      role: {
-        type: "string",
-        defaultValue: "USER",
-        required: false,
-      },
-      phone: {
-        type: "string",
-        required: false,
-      },
-      status: {
-        type: "string",
-        defaultValue: "ACTIVE",
-        required: false,
-      },
+
+  trustedOrigins: [process.env.APP_URL!, "http://localhost:3000"],
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      prompt: "select_account consent",
+      // accessType: "offline",
     },
   },
+
+  user: {
+    additionalFields: {
+      role: { type: "string", defaultValue: "USER", required: false },
+      phone: { type: "string", required: false },
+      status: { type: "string", defaultValue: "ACTIVE", required: false },
+    },
+  },
+
   emailAndPassword: {
     enabled: true,
     autoSignIn: false,
     requireEmailVerification: true,
   },
+
   emailVerification: {
     sendOnSignUp: true,
     autoSignInAfterVerification: true,
@@ -62,19 +64,12 @@ export const auth = betterAuth({
         console.error("Error sending welcome email:", error);
       }
     },
-    socialProviders: {
-      google: {
-        prompt: "select_account consent",
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        accessType: "offline",
-      },
-    },
   },
+
   advanced: {
     cookies: {
       session_token: {
-        name: "session_token", // Force this exact name
+        name: "session_token",
         attributes: {
           httpOnly: true,
           secure: true,
@@ -82,13 +77,13 @@ export const auth = betterAuth({
           partitioned: true,
         },
       },
+
       state: {
-        name: "session_token", // Force this exact name
+        name: "oauth_state",
         attributes: {
           httpOnly: true,
           secure: true,
-          sameSite: "none",
-          partitioned: true,
+          sameSite: "lax",
         },
       },
     },
